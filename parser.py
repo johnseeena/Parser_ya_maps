362460f4d7eb29ed2d35ccf5618d67881ceafa69
johnseeena <82346607+johnseeena@users.noreply.github.com>
2023-05-23 11:50:42 +0300

    worked version

--- parser.py
+++ parser.py
@@ -1,22 +1,33 @@
-from telnetlib import EC
+import time
+
+from requests import Session
+from selenium.webdriver.support import expected_conditions as EC
 from parser_class import Review
+
 import driver as driver
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver import ActionChains
+import requests
+from bs4 import BeautifulSoup
+from selenium.common.exceptions import (NoSuchElementException, ElementNotInteractableException)
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import requests
-import time
-from selenium.common.exceptions import (NoSuchElementException, ElementNotInteractableException)
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium_stealth import stealth
 
 
-def search_bar_text_input(address_postamat):
-    search_box = driver.find_element_by_css_selector(".input__control._bold")
-    search_box.send_keys("Почтовый терминал " + address_postamat)
+def search_bar_text_input(address_post):
+    search_box = driver.find_element(By.CSS_SELECTOR, ".input__control._bold")
+    search_box.send_keys("Почтовый терминал " + address_post)
     search_box.send_keys(Keys.ENTER)
     time.sleep(5)
+
+
+def star_count(self, element):
+    default_count = 0
+    for star in element.find_elements(By.XPATH, './/*[@class="business-rating-badge-view__stars"]/.//span'):
+        if "empty" not in star.get_attribute("class"):
+            default_count += 1
+    return default_count
 
 
 def parser_url():
@@ -29,51 +40,51 @@ def parser_url():
     print("Прошел первую кт")
     while True:
         try:
-            time.sleep(3) #Иначе он крутит страницу быстрее и появляються дубли отзывов
+            time.sleep(3)
+            # Иначе он крутит страницу быстрее и появляються дубли отзывов
             view_more_button = driver.find_element(By.CLASS_NAME, 'tabs-select-view__title._name_reviews')
-            print("Вторая кт")
-            #WebDriverWait(driver, 10).until_not(EC.presence_of_element_located((By.XPATH, '//*[@class="orgpage-reviews-view__loader"]')))
+            print("Вторая кт " + view_more_button.text)
+            WebDriverWait(driver, 10).until_not(EC.presence_of_element_located((By.XPATH, '//*[@class="orgpage-reviews-view__loader"]')))
             view_more_button.click()
 
         except NoSuchElementException:
-            print('К сожалению, пока отзывов и оценок нет, надеемся они скоро появятся.' )
+            print('К сожалению, пока отзывов и оценок нет, надеемся они скоро появятся.')
             break
 
-    more_buttons = driver.find_elements_by_xpath('//*[@class="business-review-view__expand"]')
-
-    for more_button in more_buttons:
-
         try:
-            driver.execute_script("arguments[0].scrollIntoView(true);", more_button)
-            time.sleep(1)
-            more_button.click()
+            more_buttons = driver.find_elements(By.CSS_SELECTOR, '//*[@class="business-review-view__expand"]')
+            print(more_buttons)
 
-        except ElementNotInteractableException:
-            pass
+            for more_button in more_buttons:
 
-def get_name(soup_content):
-    for data in soup_content.find_all("h1", {"class": "orgpage-header-view__header"}):
-        name = data.getText()
-    return name
+                try:
+                    driver.execute_script("arguments[0].scrollIntoView(true);", more_button)
+                    time.sleep(1)
+                    more_button.click()
+
+                except ElementNotInteractableException:
+                    pass
+        except:
+            print("Ну нет этой кнопки нуу нет !")
 
 
-"""def get_reviews(soup_content):
-    try:
-        time.sleep(5)
-        driver.find_element(By.CLASS_NAME, 'tabs-select-view__title._name_reviews').click()
+        reviews = driver.find_elements(By.XPATH, '//*[@class="business-review-view__info"]')
 
-    except:
-        print("Для данного постамата отзывов и оценок пока нет!")
+        for review in reviews:
+            driver.execute_script("arguments[0].scrollIntoView(true);", review)
+            new_review = Review(review).__dict__
+            #подвязать к бд!
+            print(new_review)
 
-"""
+
 def search_elements():
     element = driver.find_element(By.CLASS_NAME, 'search-business-snippet-view__content')
-    time.sleep(5)
+    time.sleep(2)
     element.click()
-    time.sleep(5)
+    time.sleep(2)
     parser_url()
     driver.back()
-    time.sleep(5)
+    time.sleep(2)
 
 
 if __name__ == '__main__':
@@ -85,9 +96,25 @@ if __name__ == '__main__':
     driver = webdriver.Chrome('chromedriver.exe', options=options)
     и закомментируй следующую строку
     """
-    driver = webdriver.Chrome('chromedriver.exe')
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+
+    driver = webdriver.Chrome('chromedriver.exe', options=options)
+
+    stealth(driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
+
     driver.get('https://yandex.ru/maps/')
-    address_postamat = input("Адрес постамата!")
-    search_bar_text_input(address_postamat)
+    address_post = input("Адрес постамата!")
+    search_bar_text_input(address_post)
     search_elements()
     driver.quit()
--- parser_class.py
+++ parser_class.py
@@ -1,15 +1,13 @@
-from selenium.common.exceptions import (
-    NoSuchElementException,
-)
+from selenium.common.exceptions import (NoSuchElementException)
 from datetime import datetime
 import pytz
+from selenium.webdriver.common.by import By
 
 utc = pytz.timezone("UTC")
 
+
 # Это класс, с отзывом.
 class Review(object):
-    "class for reviews"
-
     # Вывод словаря атрибутов
     def __repr__(self):
         return repr(self.__dict__)
@@ -17,9 +15,7 @@ class Review(object):
     # Расчет рейтинга
     def star_count(self, element):
         default_count = 0
-        for star in element.find_elements_by_xpath(
-            './/*[@class="business-rating-badge-view__stars"]/.//span'
-        ):
+        for star in element.find_elements(By.XPATH, './/*[@class="business-rating-badge-view__stars"]/.//span'):
             if "empty" not in star.get_attribute("class"):
                 default_count += 1
         return default_count
@@ -27,26 +23,23 @@ class Review(object):
     # Проверка наличия ответа на отзыв.
     def check_business_response(self, element):
         try:
-            business_review = element.find_element_by_xpath(
-                './/*[@class="business-review-view__comment"]'
-            )
+            business_review = element.find_element(By.XPATH, './/*[@class="business-review-view__comment"]')
             business_review.click()
-            return BusinessResponse(business_review)
-        except NoSuchElementException:
-            return BusinessResponse(None)
+            return business_review
 
-    # Основная ф-ция инициализации, которая принимает <selenium.webdriver.firefox.webelement.FirefoxWebElement> на вход с отзывом
+        except NoSuchElementException:
+            return None
+
+    # Основная ф-ция инициализации, которая принимает <selenium.webdriver.chrome.webelement.ChromeWebElement> на
+    # вход с отзывом
     def __init__(self, element):
         try:
             # ID автора
-            self.author_id = (
-                element.find_element_by_css_selector(
-                    "a.user-icon-view"
-                )
-                .get_attribute("href")
-                .replace("https://yandex.ru/ugcpub/pk/", "")
-                .replace("http://yandex.ru/ugcpub/pk/", "")
-            )
+            self.author_id = (element.find_element(By.CSS_SELECTOR, r"a.user-icon-view")
+                              .get_attribute("href")
+                              .replace("https://yandex.ru/ugcpub/pk/", "")
+                              .replace("http://yandex.ru/ugcpub/pk/", "")
+                              )
             # self.author_id = (
             #     element.find_element_by_xpath('.//*[@class="user-icon-view"]')
             #         .get_attribute("href")
@@ -57,81 +50,59 @@ class Review(object):
             pass
         try:
             # Имя пользователя
-            self.author_name = element.find_element_by_xpath(
-                './/*[@class="business-review-view__author"]/.//span'
-            ).text
+            self.author_name = element.find_element(By.XPATH, './/*[@class="business-review-view__author"]/.//span').text
+
         except NoSuchElementException:
             pass
+
         try:
             # Уровень пользователя
-            self.author_profession = int(
-                "".join(
-                    filter(
-                        str.isdigit,
-                        element.find_element_by_xpath(
-                            './/*[@class="business-review-view__author-profession"]'
-                        ).text,
-                    )
-                )
-            )
+            self.author_profession = int("".join(filter(str.isdigit, element.find_element(By.XPATH, './/*[@class="business-review-view__author-profession"]').text)))
+
         except NoSuchElementException:
             pass
+
         try:
             # Оценка пользователя
             self.stars = self.star_count(element)
+
         except NoSuchElementException:
             pass
+
         try:
             # Дата и время отправки отзыва !!!ПРОВЕРИТЬ ТАЙМЗОНУ!!!
-            self.datetime = datetime.strptime(
-                element.find_element_by_xpath(
-                    './/*[@class="business-review-view__date"]//meta[@itemprop="datePublished"]'
-                ).get_attribute("content"),
-                "%Y-%m-%dT%H:%M:%S.%fZ",
-            ).astimezone(utc)
+            self.datetime = datetime.strptime(element.find_element(By.XPATH, './/*[@class="business-review-view__date"]//meta[@itemprop="datePublished"]').get_attribute("content"), "%Y-%m-%dT%H:%M:%S.%fZ").astimezone(utc)
+
         except NoSuchElementException:
             pass
+
         try:
-            self.text = element.find_element_by_xpath(
-                './/*[@class="business-review-view__body"]'
-            ).text.replace("\n", "")
+            self.text = element.find_element(By.XPATH, './/*[@class="business-review-view__body"]').text.replace("\n", "")
+
         except NoSuchElementException:
             pass
+
         try:
             self.like = int(
-                element.find_element_by_xpath(
-                    './/*[@class="business-reactions-view__icon"]/following-sibling::*'
-                ).text
-            )
+                element.find_element(By.XPATH, './/*[@class="business-reactions-view__icon"]/following-sibling::*').text)
+
         except NoSuchElementException:
             self.like = 0
+
         try:
-            self.dislike = int(
-                element.find_element_by_xpath(
-                    './/*[@class="business-reactions-view__icon _dislike"]/following-sibling::*'
-                ).text
-            )
+            self.dislike = int(element.find_element(By.XPATH, './/*[@class="business-reactions-view__icon _dislike"]/following-sibling::*').text)
+
         except NoSuchElementException:
             self.dislike = 0
         try:
-            business_review = element.find_element_by_xpath(
-                './/*[@class="business-review-view__comment"]'
-            )
+            business_review = element.find_element(By.XPATH, './/*[@class="business-review-view__comment"]')
             business_review.click()
 
             self.response_flag = True
             # Название организации
-            self.response_title = element.find_element_by_xpath(
-                './/*[@class="business-review-view__comment-title"]'
-            ).text
             # Дата и время отправки отзыва !!!ПРОВЕРИТЬ ТАЙМЗОНУ!!!
-            self.response_datetime = element.find_element_by_xpath(
-                './/*[@class="business-review-view__date _org-answer"]'
-            ).text
-            self.response_text = element.find_element_by_xpath(
-                './/*[@class="business-review-view__body _org-answer"]'
-            ).text.replace(
-                "\n", ""
-            )
+            self.response_datetime = element.find_element(By.XPATH, './/*[@class="business-review-view__date _org-answer"]').text
+            self.response_text = element.find_element(By.XPATH, './/*[@class="business-review-view__body _org-answer"]').text.replace("\n", "")
+
         except NoSuchElementException:
             pass
